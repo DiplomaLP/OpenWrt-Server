cc31d0716c66e1bd57a4749e426b7ad6ecc244df
yoopibit <yavorskyi.yurii@gmail.com>
2018-03-17 17:10:09 +0200

    update the CMakeLists.txt

---
+++ server.c
@@ -0,0 +1,5 @@
+//
+// Created by yura on 17.03.18.
+//
+
+#include "server.h"
---
+++ server.h
@@ -0,0 +1,8 @@
+//
+// Created by yura on 17.03.18.
+//
+
+#ifndef OPENWRT_SERVER_SERVER_H
+#define OPENWRT_SERVER_SERVER_H
+
+#endif //OPENWRT_SERVER_SERVER_H
