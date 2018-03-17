aa56f372d8d9efb1d42966296a1f4c0023d4b394
yoopibit <yavorskyi.yurii@gmail.com>
2018-03-17 17:07:33 +0200

    add build script for openWrt

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+cd build/Debug/
+
+STAGING_DIR=~/open_wrt/openwrt/staging_dir/toolchain-mips_24kc_gcc-5.5.0_musl
+
+PATH=$PATH:$STAGING_DIR/bin
+
+$STAGING_DIR/bin/mips-openwrt-linux-gcc ../../main.c -o server -static
+
+ret_code=$?
+if [ $ret_code != 0 ]; then
+  printf "Error : [%d] when executing command: 'gcc'" $ret_code
+  exit $ret_code
+else
+  printf "The program compiled successfully\n"
+fi
+
+scp server root@GL-AR150:/root/test/Server/server
+
+ret_code=$?
+if [ $ret_code != 0 ]; then
+  printf "Error : [%d] when executing command: 'scp'" $ret_code
+  exit $ret_code
+else
+  printf "The program transferred successfully\n"
+fi
